9f761f845040b3f435c2704e528cf75ff6b77951
Alexander Voronkov <ax.voronkov@gmail.com>
2021-11-10 04:42:50 +0300

    Upd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import Countdown from 'react-countdown';
+
+const Completionist = () => <span>НА ВСЕХ ПЛОЩАДКАХ</span>;
 
 function App() {
   return (
@@ -10,18 +12,43 @@ function App() {
     </video>
     <div className="App">
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Countdown className="App-countdown" date={Date.parse("November 25, 2021")}>
+          <Completionist />
+        </Countdown>
+        <div className="App-links">
+          {/* <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="App-link" 
+          >
+            <img src="soundcloud.png" alt="soundcloud"/>
+          </a> */}
+          <a
+            href="https://vk.com/ubivayunasekomih"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="App-link" 
+          >
+            <img src="vk.png" alt="vk"/>
+          </a>
+          <a
+            href="https://open.spotify.com/artist/5LgnxGyp7XFimXuoZuG9zd"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="App-link" 
+          >
+            <img src="spotify.png" alt="spotify"/>
+          </a>
+          <a
+            href="https://www.instagram.com/ubivayunasekomih/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="App-link" 
+          >
+            <img src="instagram.png" alt="instagram"/>
+          </a>
+        </div>
       </div>
     </div>
     </React.Fragment>
